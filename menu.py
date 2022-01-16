96ffdad15d97062653ce01eecc862b6651e02017
RealSteel2022 <callum.a-r@hotmail.co.uk>
2022-01-16 18:35:27 +0000

    Completed a new iteration of menu display making it module to simplify

--- menu.py
+++ menu.py
@@ -1,50 +1,30 @@
-def menu():
-    import random
-    from AvailableQuestions import AvailableQuestions
+def menu_service():
+    menu_options()
 
-    storedQuestions = [["When did Nixon become president?", "1969"], ["How many wives did Henry VIII have?", "6"],
-                       ["How big was Henry VIII's privy council?", "12 members"], ]
 
-    q1 = AvailableQuestions("When did Nixon become president?", "1969")
-    # q2 = availableQuestions("How many wives did Henry VIII have?", "6")
-    # q3 = availableQuestions("How big was Henry VIII's privy council?", "12 members")
+def menu_options():
+    print("Test - If you would like to question test then select: 1")
+    print("Display Questions - If you would like to display stored questions select: 2")
+    print("Exit - If you would like to exit the program select: 3")
+    menu_selection()
+    # 1 input questions and answers
+    # 2 take a test
 
-    # testing question and answers can be displayed
-    print(q1.question, q1.answer)
 
-    def menuChoice():
-        return input("Press 1")
+def menu_selection():
+    user_choice = input("\n")
+    if user_choice == "1":
+        return print("User choice is " + user_choice)  # call the question tester
+    elif user_choice == "2":
+        print("User choice is " + user_choice)  # call the shower
+    elif user_choice == "3":
+        print("About to exit")
+        menu_exit()
+    else:
+        print("Invalid input")
+        menu_options()
 
-    viewed = [""]
-    length = len(storedQuestions)
-    sessionFinish = False
 
-    i = 0
-
-    while menuChoice != "end" or "End":
-        menuChoice()
-        if menuChoice() == "1":
-            if sessionFinish == "True":
-                storedQuestions = viewed
-                print(storedQuestions)
-            else:
-                while storedQuestions != []:
-                    randomQuestion = random.randint(0, (length - 1))
-                    viewed = viewed.update(storedQuestions[randomQuestion])
-                    print(storedQuestions[randomQuestion][0])
-                    answer = input("Please input the answer ")
-                    if answer == storedQuestions[randomQuestion][1]:
-                        print("Correct well done.")
-                        del storedQuestions[randomQuestion]
-                        length = length - 1
-                        if storedQuestions == []:
-                            sessionFinish = "True"
-                            print("Done")
-                    else:
-                        print("Incorrect the answer was " + storedQuestions[randomQuestion][1])
-                    i = length - 1
-        elif menuChoice() == "2":
-            print(storedQuestions)
-
-        # hello world
-        # intelliJ changes
+def menu_exit():
+    print("exit menu selection")
+    exit()
