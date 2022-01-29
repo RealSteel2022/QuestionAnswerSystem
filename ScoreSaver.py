44fa9684b132439354b39c3af4e93a78917b81ac
RealSteel2022 <callum.a-r@hotmail.co.uk>
2022-01-29 18:39:12 +0000

    Allows user to practice questions and leaderboard system to temporarily compare the score to an already stored score to tell the user if they have improved

---
+++ ScoreSaver.py
@@ -0,0 +1,16 @@
+from QuestionShower import *
+
+temporary_lb_score = 2
+session_score = 0
+
+
+def scored_point():
+    global session_score
+    session_score += 1
+
+
+def leaderboard_score():
+    if session_score > temporary_lb_score:
+        print("New high-score")
+    else:
+        print("You were unable to beat your previous high-score of " + str(temporary_lb_score))
