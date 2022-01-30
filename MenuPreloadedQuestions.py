0527e91f65203d851308367f292bfa8b0e3c3f6e
RealSteel2022 <callum.a-r@hotmail.co.uk>
2022-01-30 13:15:25 +0000

    During show and tell stakeholder asked whether they could also have some pre-set questions to work on as well as inputting their own questions so I've added a pre-load option and an extra sub-menu to get to it in order not to clutter up my original menu.

---
+++ MenuPreloadedQuestions.py
@@ -0,0 +1,33 @@
+from QuestionAndAnswerDB import pre_load_maths, pre_load_history
+
+
+def pre_load_menu_service():
+    pre_load_menu_options()
+
+
+def pre_load_menu_options():
+    print("To preload Maths press: 1")
+    print("To preload History press: 2")
+    print("Exit - If you would like to exit the program select: 3")
+    pre_load_menu_selection()
+
+
+def pre_load_menu_selection():
+    user_choice = input("\n")
+    if user_choice == "1":
+        print("User choice is " + user_choice)  # call the question tester
+        pre_load_maths()
+    elif user_choice == "2":
+        print("User choice is " + user_choice)  # call the shower
+        pre_load_history()
+    elif user_choice == "3":
+        print("Return to menu")
+    else:
+        print("Invalid input")
+        pre_load_menu_options()
+    user_choice = ""
+
+
+def menu_exit():
+    print("exit menu selection")
+    exit()
