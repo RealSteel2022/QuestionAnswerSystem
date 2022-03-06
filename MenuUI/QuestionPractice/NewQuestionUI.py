8db7462b69921bceef6fd6c089f87b1ff050e8ba
Callum <callum.a-r@hotmail.co.uk>
2022-03-06 15:21:04 +0000

    Complete re-design of the question practice UI got it to cycle through questions and look nicer need to connect it to the old code and send it through

---
+++ MenuUI/QuestionPractice/NewQuestionUI.py
@@ -0,0 +1,101 @@
+import os
+from tkinter import *
+
+directory_path = str(os.getcwd()) + "\MenuUI\QuestionPractice\\"
+
+window = Tk()
+
+stored_answer = StringVar()
+user = "my man"
+user1 = ["How many wives did Henry VIII have?", "How many members on the privy council", "Qyes"]
+my_label1 = ""
+
+current_quest = 0
+
+
+def btn_clicked():
+    print("Button Clicked")
+
+
+def practice_questions_window(username):
+    def clear_entry():
+        answer_entry.delete(0, END)
+
+    def question_update():
+        # This proves that if you have the list you can loop through teh qus here adn update with each button click
+        global current_quest
+        my_label1.config(text=user1[current_quest])
+        current_quest += 1
+        clear_entry()
+
+    window.geometry("1139x794")
+    window.configure(bg="#ffffff")
+    canvas = Canvas(
+        window,
+        bg="#ffffff",
+        height=794,
+        width=1139,
+        bd=0,
+        highlightthickness=0,
+        relief="ridge")
+    canvas.place(x=0, y=0)
+
+    background_img = PhotoImage(file=directory_path + "background.png")
+    background = canvas.create_image(
+        569.5, 397.0,
+        image=background_img)
+
+    img0 = PhotoImage(file=directory_path + "img0.png")
+    b0 = Button(
+        image=img0,
+        borderwidth=0,
+        highlightthickness=0,
+        command=btn_clicked,
+        relief="flat")
+
+    b0.place(
+        x=926, y=707,
+        width=187,
+        height=65)
+
+    img1 = PhotoImage(file=directory_path + "img1.png")
+    answer_entry = Entry(
+        window,
+        bg="#58bbc2",
+        font=("Spartan-Regular", int(24.0)),
+        borderwidth=0,
+        textvariable=stored_answer,
+        highlightthickness=0,
+        relief="flat")
+
+    answer_entry.place(
+        x=401, y=405,
+        width=338,
+        height=79)
+
+    img2 = PhotoImage(file=directory_path + "img2.png")
+    b2 = Button(
+        image=img2,
+        borderwidth=0,
+        highlightthickness=0,
+        command=question_update,
+        relief="flat")
+
+    b2.place(
+        x=496, y=561,
+        width=147,
+        height=50)
+
+    global my_label1
+    my_label1 = Label(
+        window,
+        text=user,
+        bg="#fff1a5",
+        fg="#737373",
+        font=("Eczar-SemiBold", int(24.0)))
+    my_label1.place(x=319.5, y=286.5)
+    window.resizable(False, False)
+    my_label1.config(text=username)
+
+    window.resizable(False, False)
+    window.mainloop()
--- com/qa/system/QuestionPracticer.py
+++ com/qa/system/QuestionPracticer.py
@@ -1,7 +1,4 @@
 from com.qa.system.QuestionAndAnswerDB import get_questions_answers
-from com.qa.system.ScoreService import scored_point, leaderboard_score
-import time
-
 
 
 # def practice_questions():
@@ -15,8 +12,8 @@ import time
 #     leaderboard_score()
 
 def calling_practice_window():
-    import MenuUI.QuestionPractice.QuestionUI
-    MenuUI.QuestionPractice.QuestionUI.practice_questions_window("Callum")
+    import MenuUI.QuestionPractice.old_ui.QuestionUI
+    MenuUI.QuestionPractice.old_ui.QuestionUI.practice_questions_window("Callum")
     # this proves you can send some data from here to the gui module
     # from MenuUI.QuestionPractice.QuestionUI import practice_questions_window
 
@@ -25,8 +22,8 @@ def practice_questions():
     for questions_answer in get_questions_answers():
         ui_question_display = questions_answer.display_question()
         print(ui_question_display)
-        import MenuUI.QuestionPractice.QuestionUI
-        MenuUI.QuestionPractice.QuestionUI.practice_questions_window(ui_question_display)
+        import MenuUI.QuestionPractice.NewQuestionUI
+        MenuUI.QuestionPractice.NewQuestionUI.practice_questions_window(ui_question_display)
         # calling_practice_window()
         # from MenuUI.QuestionPractice.QuestionUI import practice_questions_window
         # practice_questions_window("Callum")
