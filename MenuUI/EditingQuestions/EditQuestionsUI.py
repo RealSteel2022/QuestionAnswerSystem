4a117c3d56499614a5ba4b1ec466dfa4b8ce6c56
RealSteel2022 <callum.a-r@hotmail.co.uk>
2022-04-14 19:11:10 +0100

    Newer adding questions page

--- MenuUI/EditingQuestions/EditQuestionsUI.py
+++ MenuUI/EditingQuestions/EditQuestionsUI.py
@@ -10,71 +10,73 @@ def back_btn_clicked():
 directory_path = str(os.getcwd()) + "\MenuUI\EditingQuestions\\"
 
 
-def subject_btn_clicked():
-    print("Subject clicked")
+def add_btn_clicked():
+    window.withdraw()
+    os.system("MenuUI\AddingQuestionUI\QuestionAddingUI.py")
+    print("Add clicked")
 
 
-def questions_btn_clicked():
-    print("Questions clicked")
+def delete_btn_clicked():
+    print("Delete clicked")
 
 
 window = Tk()
 
 window.geometry("1139x794")
-window.configure(bg="#ffffff")
+window.configure(bg = "#ffffff")
 canvas = Canvas(
     window,
-    bg="#ffffff",
-    height=794,
-    width=1139,
-    bd=0,
-    highlightthickness=0,
-    relief="ridge")
-canvas.place(x=0, y=0)
+    bg = "#ffffff",
+    height = 794,
+    width = 1139,
+    bd = 0,
+    highlightthickness = 0,
+    relief = "ridge")
+canvas.place(x = 0, y = 0)
 
-background_img = PhotoImage(file=directory_path + "background.png")
+background_img = PhotoImage(file = directory_path + "background.png")
 background = canvas.create_image(
     569.5, 397.0,
     image=background_img)
 
-img0 = PhotoImage(file=directory_path + "img0.png")
+img0 = PhotoImage(file = directory_path + "img0.png")
 b0 = Button(
-    image=img0,
-    borderwidth=0,
-    highlightthickness=0,
-    command=subject_btn_clicked,
-    relief="flat")
+    image = img0,
+    borderwidth = 0,
+    highlightthickness = 0,
+    command = delete_btn_clicked,
+    relief = "flat")
 
 b0.place(
-    x=326, y=347,
-    width=215,
-    height=100)
+    x = 602, y = 359,
+    width = 251,
+    height = 103)
 
-img1 = PhotoImage(file=directory_path + "img1.png")
+img1 = PhotoImage(file = directory_path + "img1.png")
 b1 = Button(
-    image=img1,
-    borderwidth=0,
-    highlightthickness=0,
-    command=questions_btn_clicked,
-    relief="flat")
+    image = img1,
+    borderwidth = 0,
+    highlightthickness = 0,
+    command = add_btn_clicked,
+    relief = "flat")
 
 b1.place(
-    x=621, y=347,
-    width=216,
-    height=100)
+    x = 311, y = 357,
+    width = 267,
+    height = 109)
 
-img2 = PhotoImage(file=directory_path + "img2.png")
+img2 = PhotoImage(file = directory_path + "img2.png")
 b2 = Button(
-    image=img2,
-    borderwidth=0,
-    highlightthickness=0,
-    command=back_btn_clicked,
-    relief="flat")
+    image = img2,
+    borderwidth = 0,
+    highlightthickness = 0,
+    command = back_btn_clicked,
+    relief = "flat")
 
 b2.place(
-    x=926, y=707,
-    width=187,
-    height=65)
+    x = 926, y = 707,
+    width = 187,
+    height = 65)
 
 window.resizable(False, False)
 window.mainloop()
